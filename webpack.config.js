d459bfa2ebfff0d78a19edfa8f36f8878cc26595
jrozal <68182930+jrozal@users.noreply.github.com>
2021-02-23 21:15:40 -0800

    Change webpack mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, 'public');
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'navbar_bundle.js',
