d7a366f1454de937366bcdc4f1d98457cffcc09e
jrozal <68182930+jrozal@users.noreply.github.com>
2021-02-23 23:06:07 -0800

    Add middleware to return js.gz compression bundle

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const port = 4008;
 const cors = require('cors');
 
 app.use(cors());
+
+app.get('*.js', function (req, res, next) {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  next();
+});
+
 app.use('/', express.static(__dirname + '/../public'));
 app.use('/:id', express.static(__dirname + '/../public'));
 
